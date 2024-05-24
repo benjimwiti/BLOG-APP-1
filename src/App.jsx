68c75b33f29dbd9b127509f9bf462520230db7e2
benjimwiti <167991485+benjimwiti@users.noreply.github.com>
2024-05-24 18:48:16 +0300

    isAuth is now based on a local storage check , created createNewPost ref remotely , defined an object stucture for createPostfn

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,17 @@ import viteLogo from '/vite.svg'
 import Home from "./pages/home"
 import Login from "./pages/login"
 import Main from "./pages/create-post"
+import ErrorSpecs from './pages/error-specs'
 
 import { signOut } from 'firebase/auth'
 import {auth} from './firebase-config'
+import { getAuth, setPersistence } from 'firebase/auth'
 
 
 import {BrowserRouter as Router , Routes , Route , Link } from "react-router-dom"
 import './App.css'
+import firebase from 'firebase/compat/app'
+import CreatePost from './pages/create-post'
 
 function Layout () {
   return (
@@ -19,16 +23,29 @@ function Layout () {
     </div>
   )
 }
-function App() {
-    const [isAuth , setIsAuth] = useState(true)
 
+function App() {
+    const loggedIn = localStorage.getItem('isAuth') || false 
+    const [isAuth , setIsAuth] = useState(loggedIn)
+    console.log(loggedIn)
+    //if()
+    const [errorInfo, setErrorInfo] = useState("")
+    console.log(auth)
     //sign out functionality --after promise resolves change isAuth state to apply the changes
-    const signOutfn = () => {
-      signOut(auth).then(() => {
-        localStorage.clear()
-        setIsAuth(false) 
-      })
+    const signOutfn = async () => {
+      try {
+        await signOut(auth)
+          localStorage.clear()
+          setIsAuth(false) 
+        
+      } catch (error) {
+        setErrorInfo (error)
+        window.location.pathname = "*"
+        console.log('unsuccessful log out')
+      }
+      
     }
+    
 
   return (
     <>
@@ -37,9 +54,12 @@ function App() {
        <nav>
           <Link to="/" > Home </Link>
           {!isAuth ? 
-           <Link to="/login" > Login </Link> :
-            <button className='sign-out-btn' onClick={signOutfn} > Sign Out</button> }
-          <Link to="/main" > Main </Link>
+            <Link to="/login" > Login </Link> :
+          <>
+            <Link to="/create-post" > Create New post </Link>
+            <button className='sign-out-btn' onClick={signOutfn} > Sign Out</button>
+          </>
+          }
        </nav>
       
       <Routes>
@@ -47,8 +67,8 @@ function App() {
         
           <Route path='/' element={<Home />} />
           <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
-          <Route path="/main" element={<Main />} />
-        
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path = '*' element = {<ErrorSpecs errorInfoProp={errorInfo}/>} />
 
       </Routes>
     
